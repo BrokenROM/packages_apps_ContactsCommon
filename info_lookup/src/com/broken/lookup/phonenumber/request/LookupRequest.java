84192cc987ed279db5c38bac97b62da564aab7de
Rohit Yengisetty <rohit@cyngn.com>
2015-06-16 16:58:55 -0700

    Add generic hooks for contact info lookup based on phone number
    Change-Id: Ic66c936fb29579724c930232f5cc1ef2ae135313

---
+++ info_lookup/src/com/broken/lookup/phonenumber/request/LookupRequest.java
@@ -0,0 +1,25 @@
+package com.broken.lookup.phonenumber.request;
+
+import com.broken.lookup.phonenumber.response.LookupResponse;
+
+/**
+ * Encapsulates the notion of a phone number lookup request
+ */
+public class LookupRequest {
+    public String mPhoneNumber;
+    public Callback mCallback;
+
+    public LookupRequest(String phoneNumber, Callback callback) {
+        mPhoneNumber = phoneNumber;
+        mCallback = callback;
+    }
+
+    @Override
+    public int hashCode() {
+        return mPhoneNumber.hashCode();
+    }
+
+    public interface Callback {
+        void onNewInfo(LookupRequest lookupRequest, LookupResponse response);
+    }
+}
